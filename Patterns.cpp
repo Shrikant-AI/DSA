eb0f82360630b7e3377d272c653af21338febb16
Shrikant Shinde <114386864+Shrikant-AI@users.noreply.github.com>
2024-08-07 18:31:34 +0530

    Update Patterns.cpp

--- Patterns.cpp
+++ Patterns.cpp
@@ -2,4 +2,25 @@
 #include<string>
 using namespace std;
 
-// Pattern 1 :
+/* Pattern 1 :
+For n = 2 the pattern will be 
+2 2 1 1
+2 1
+
+For n = 3 the pattern will be 
+3 3 3 2 2 2 1 1 1
+3 3 2 2 1 1
+3 2 1
+*/
+
+void pattern1(int n) {
+    for (int i=n;i>0;i--){
+        for(int j=n;j>0;j--){
+            for(int k=0;k<i;k++)
+            {
+                cout<<j<<" ";
+            }
+        }
+        cout<<endl;
+    }
+}
