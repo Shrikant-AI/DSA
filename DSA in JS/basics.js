db9ed8d2d64146dd318d9280368f84a9b69b8f1e
Shrikant <shindeshrikant583@gmail.com>
2024-11-07 21:32:31 +0530

    "Added nested loop to findPairs function, uncommented function call, and added reverseString function to reverse a given string."

--- DSA in JS/basics.js
+++ DSA in JS/basics.js
@@ -13,6 +13,27 @@ const numbers = [1,2,3,4,5];
 
 const findPairs =(arr)=>{
    for(let i=0;i<arr.length;i++){
-      
+      for(let j=0;j<arr.length;j++){
+         console.log(`Pair: ${arr[i]},${arr[j]}`)
+      }
    }
 }
+
+// findPairs(numbers);
+
+
+//** Watch from 00:30 min => how to create */
+
+//* Reverse a String =>
+
+const reverseString = (string)=>{
+   let splitString = string.split("");
+   // console.log(`Split String is ${splitString}`)
+   let reverseString = splitString.reverse();
+   let finalString = reverseString.join("");
+   console.log(`Reversed String is ${finalString}`)
+   console.log(`${finalString===string?true:false}`)
+}
+
+
+reverseString ('JALAJ')
